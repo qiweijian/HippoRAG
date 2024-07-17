6ffb1eddd09a7aeab8f38ac23adae91006c6b1ac
Weijian QI <1477825606@qq.com>
2024-07-17 08:12:02 +0000

    change

--- src/ircot_hipporag_v3.py
+++ src/ircot_hipporag_v3.py
@@ -7,13 +7,14 @@ from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
 
 from src.langchain_util import init_langchain_model
-from transformers.hf_argparser import string_to_bool
-import argparse
+from transformers.hf_argparser import HfArgumentParser
+from dataclasses import dataclass, field
+from typing import Any, Tuple
 import json
 
 from tqdm import tqdm
 
-from hipporag import HippoRAG
+from hipporag_v3 import HippoRAGv3, HipporagConfig
 
 ircot_reason_instruction = 'You serve as an intelligent assistant, adept at facilitating users through complex, multi-hop reasoning across multiple documents. This task is illustrated through demonstrations, each consisting of a document set paired with a relevant question and its multi-hop reasoning thoughts. Your task is to generate one thought for current step, DON\'T generate the whole thoughts at once! If you reach what you believe to be the final step, start with "So the answer is:".'
 
@@ -45,7 +46,7 @@ def parse_prompt(file_path):
     return parsed_data
 
 
-def retrieve_step(query: str, corpus, top_k: int, rag: HippoRAG, dataset: str):
+def retrieve_step(query: str, corpus, top_k: int, rag: HippoRAGv3, dataset: str):
     ranks, scores, logs = rag.rank_docs(query, top_k=top_k)
     if dataset in ['hotpotqa', 'hotpotqa_train']:
         retrieved_passages = []
@@ -107,89 +108,77 @@ def reason_step(dataset, few_shot: list, query: str, passages: list, thoughts: l
     return response_content
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str)
-    parser.add_argument('--llm', type=str, default='openai', help="LLM, e.g., 'openai' or 'together'")
-    parser.add_argument('--llm_model', type=str, default='gpt-3.5-turbo-1106', help='Specific model name')
-    parser.add_argument('--retriever', type=str, default='facebook/contriever')
-    parser.add_argument('--prompt', type=str)
-    parser.add_argument('--num_demo', type=int, default=1, help='the number of demo samples')
-    parser.add_argument('--max_steps', type=int)
-    parser.add_argument('--top_k', type=int, default=8, help='retrieving k documents at each step')
-    parser.add_argument('--doc_ensemble', type=str, default='t')
-    parser.add_argument('--dpr_only', type=str, default='f')
-    parser.add_argument('--graph_alg', type=str, default='ppr')
-    parser.add_argument('--wo_node_spec', action='store_true')
-    parser.add_argument('--sim_threshold', type=float, default=0.8)
-    parser.add_argument('--recognition_threshold', type=float, default=0.9)
-    parser.add_argument('--damping', type=float, default=0.1)
-    parser.add_argument('--force_retry', action='store_true')
-    args = parser.parse_args()
+@dataclass
+class ircotConfig(HipporagConfig):
+    prompt: str = field(
+        default=None,
+        metadata={'help': 'The prompt to use'}
+    )
+    num_demo: int = field(
+        default=1,
+        metadata={'help': 'The number of demo samples'}
+    )
+    max_steps: int = field(
+        default=None,
+        metadata={'help': 'Maximum number of steps'}
+    )
+    top_k: int = field(
+        default=8,
+        metadata={'help': 'Retrieving k documents at each step'}
+    )
+    force_retry: bool = field(
+        default=False,
+        metadata={'help': 'Force retry if necessary'}
+    )
 
-    # Please set environment variable OPENAI_API_KEY
-    doc_ensemble = string_to_bool(args.doc_ensemble)
-    dpr_only = string_to_bool(args.dpr_only)
-
-    client = init_langchain_model(args.llm, args.llm_model)
-    llm_model_name_processed = args.llm_model.replace('/', '_').replace('.', '_')
-    # if args.llm_model == 'gpt-3.5-turbo-1106':  # Default OpenIE system
-    #     colbert_configs = {'root': f'data/lm_vectors/colbert/{args.dataset}', 'doc_index_name': 'nbits_2', 'phrase_index_name': 'nbits_2'}
-    # else:
-    #     colbert_configs = {'root': f'data/lm_vectors/colbert/{args.dataset}_{llm_model_name_processed}', 'doc_index_name': 'nbits_2', 'phrase_index_name': 'nbits_2'}
-    colbert_configs = {'root': f'data/lm_vectors/colbert/{args.dataset}', 'doc_index_name': 'nbits_2', 'phrase_index_name': 'nbits_2'}
-
-    rag = HippoRAG(args.dataset, args.llm, args.llm_model, args.retriever, doc_ensemble=doc_ensemble, node_specificity=not (args.wo_node_spec), sim_threshold=args.sim_threshold,
-                   colbert_config=colbert_configs, dpr_only=dpr_only, graph_alg=args.graph_alg, damping=args.damping, recognition_threshold=args.recognition_threshold)
-
-    data = json.load(open(f'data/{args.dataset}.json', 'r'))
-    corpus = json.load(open(f'data/{args.dataset}_corpus.json', 'r'))
-    max_steps = args.max_steps
-
-    if max_steps > 1:
-        if 'hotpotqa' in args.dataset:
-            prompt_path = 'data/ircot_prompts/hotpotqa/gold_with_3_distractors_context_cot_qa_codex.txt'
-        elif 'musique' in args.dataset:
-            prompt_path = 'data/ircot_prompts/musique/gold_with_3_distractors_context_cot_qa_codex.txt'
-        elif '2wikimultihopqa' in args.dataset:
-            prompt_path = 'data/ircot_prompts/2wikimultihopqa/gold_with_3_distractors_context_cot_qa_codex.txt'
+    @property
+    def output_path(self):
+        doc_ensemble_str = f'doc_ensemble_{self.recognition_threshold}' if self.doc_ensemble else 'no_ensemble'
+        dpr_only_str = 'dpr_only' if self.dpr_only else 'hipporag'
+        if self.graph_alg == 'ppr':
+            output_path = f'output/ircot_v3/ircot_results_{self.corpus_name}_{dpr_only_str}_{self.graph_creating_retriever_name_processed}_demo_{self.num_demo}_{self.extraction_model_name_processed}_{doc_ensemble_str}_step_{self.max_steps}_top_{self.top_k}_sim_thresh_{self.sim_threshold}'
+            if self.damping != 0.1:
+                output_path += f'_damping_{self.damping}'
         else:
-            prompt_path = f'data/ircot_prompts/{args.dataset}/gold_with_3_distractors_context_cot_qa_codex.txt'
+            output_path = f'output/ircot_v3/ircot_results_{self.corpus_name}_{dpr_only_str}_{self.graph_creating_retriever_name_processed}_demo_{self.num_demo}_{self.extraction_model_name_processed}_{doc_ensemble_str}_step_{self.max_steps}_top_{self.top_k}_{self.graph_alg}_sim_thresh_{self.sim_threshold}'
 
-        few_shot_samples = parse_prompt(prompt_path)[:args.num_demo]
+        if not self.node_specificity:
+            output_path += 'wo_node_spec'
+        output_path += '.json'
+        return output_path
 
-    doc_ensemble_str = f'doc_ensemble_{args.recognition_threshold}' if doc_ensemble else 'no_ensemble'
 
-    if dpr_only:
-        dpr_only_str = 'dpr_only'
-    else:
-        dpr_only_str = 'hipporag'
+def get_args() -> Tuple[ircotConfig, Any]:
+    parser = HfArgumentParser(ircotConfig)
+    return parser.parse_args_into_dataclasses(return_remaining_strings=True)
 
-    if args.graph_alg == 'ppr':
-        output_path = f'output/ircot/ircot_results_{args.dataset}_{dpr_only_str}_{rag.graph_creating_retriever_name_processed}_demo_{args.num_demo}_{llm_model_name_processed}_{doc_ensemble_str}_step_{max_steps}_top_{args.top_k}_sim_thresh_{args.sim_threshold}'
-        if args.damping != 0.1:
-            output_path += f'_damping_{args.damping}'
-    else:
-        output_path = f'output/ircot/ircot_results_{args.dataset}_{dpr_only_str}_{rag.graph_creating_retriever_name_processed}_demo_{args.num_demo}_{llm_model_name_processed}_{doc_ensemble_str}_step_{max_steps}_top_{args.top_k}_{args.graph_alg}_sim_thresh_{args.sim_threshold}'
 
-    if args.wo_node_spec:
-        output_path += 'wo_node_spec'
+if __name__ == '__main__':
+    ircot_config, unknown_args = get_args()
+    print(ircot_config)
 
-    output_path += '.json'
+    rag = HippoRAGv3(ircot_config)
+
+    data = json.load(open(ircot_config.get_path('data_path'), 'r'))
+    corpus = json.load(open(ircot_config.get_path('corpus_path'), 'r'))
+
+    if ircot_config.max_steps > 1:
+        client = init_langchain_model(ircot_config.extraction_model, ircot_config.extraction_model_name)
+        prompt_path = ircot_config.get_path('prompt_path')
+        few_shot_samples = parse_prompt(prompt_path)[:ircot_config.num_demo]
 
     k_list = [1, 2, 5, 10, 15, 20, 30, 40, 50, 80, 100]
     total_recall = {k: 0 for k in k_list}
 
-    force_retry = args.force_retry
 
-    if force_retry:
+    if ircot_config.force_retry:
         results = []
         processed_ids = set()
     else:
         try:
-            with open(output_path, 'r') as f:
+            with open(ircot_config.output_path, 'r') as f:
                 results = json.load(f)
-            if args.dataset in ['hotpotqa', '2wikimultihopqa', 'hotpotqa_train']:
+            if ircot_config.corpus_name in ['hotpotqa', '2wikimultihopqa', 'hotpotqa_train']:
                 processed_ids = {sample['_id'] for sample in results}
             else:
                 processed_ids = {sample['id'] for sample in results}
@@ -202,14 +191,14 @@ if __name__ == '__main__':
             results = []
             processed_ids = set()
 
-    print(f'Loaded {len(results)} results from {output_path}')
+    print(f'Loaded {len(results)} results from {ircot_config.output_path}')
     if len(results) > 0:
         for k in k_list:
             print(f'R@{k}: {total_recall[k] / len(results):.4f} ', end='')
         print()
 
     for sample_idx, sample in tqdm(enumerate(data), total=len(data), desc='IRCoT retrieval'):  # for each sample
-        if args.dataset in ['hotpotqa', '2wikimultihopqa', 'hotpotqa_train']:
+        if ircot_config.corpus_name in ['hotpotqa', '2wikimultihopqa', 'hotpotqa_train']:
             sample_id = sample['_id']
         else:
             sample_id = sample['id']
@@ -220,7 +209,7 @@ if __name__ == '__main__':
         query = sample['question']
         all_logs = {}
 
-        retrieved_passages, scores, logs = retrieve_step(query, corpus, args.top_k, rag, args.dataset)
+        retrieved_passages, scores, logs = retrieve_step(query, corpus, ircot_config.top_k, rag, ircot_config.corpus_name)
 
         it = 1
         all_logs[it] = logs
@@ -228,14 +217,14 @@ if __name__ == '__main__':
         thoughts = []
         retrieved_passages_dict = {passage: score for passage, score in zip(retrieved_passages, scores)}
 
-        while it < max_steps:  # for each iteration of IRCoT
-            new_thought = reason_step(args.dataset, few_shot_samples, query, retrieved_passages[:args.top_k], thoughts, client)
+        while it < ircot_config.max_steps:  # for each iteration of IRCoT
+            new_thought = reason_step(ircot_config.corpus_name, few_shot_samples, query, retrieved_passages[:ircot_config.top_k], thoughts, client)
             thoughts.append(new_thought)
             if 'So the answer is:' in new_thought:
                 break
             it += 1
 
-            new_retrieved_passages, new_scores, logs = retrieve_step(new_thought, corpus, args.top_k, rag, args.dataset)
+            new_retrieved_passages, new_scores, logs = retrieve_step(new_thought, corpus, ircot_config.top_k, rag, ircot_config.corpus_name)
             all_logs[it] = logs
 
             for passage, score in zip(new_retrieved_passages, new_scores):
@@ -251,14 +240,18 @@ if __name__ == '__main__':
         # end iteration
 
         # calculate recall
-        if args.dataset in ['hotpotqa', 'hotpotqa_train']:
+        if ircot_config.corpus_name in ['hotpotqa', 'hotpotqa_train']:
             gold_passages = [item for item in sample['supporting_facts']]
             gold_items = set([item[0] for item in gold_passages])
             retrieved_items = [passage.split('\n')[0].strip() for passage in retrieved_passages]
-        elif args.dataset in ['2wikimultihopqa']:
+        elif ircot_config.corpus_name in ['2wikimultihopqa']:
             gold_passages = [item for item in sample['supporting_facts']]
             gold_items = set([item[0] for item in gold_passages])
             retrieved_items = [passage.split('\n')[0].strip() for passage in retrieved_passages]
+        elif ircot_config.corpus_name == "musique":
+            gold_passages = [item for item in sample['paragraphs'] if item['is_supporting']]
+            gold_items = set([item['title'] + '\n' + item['paragraph_text'] for item in gold_passages])
+            retrieved_items = retrieved_passages
         else:
             gold_passages = [item for item in sample['paragraphs'] if item['is_supporting']]
             gold_items = set([item['title'] + '\n' + item['text'] for item in gold_passages])
@@ -279,7 +272,7 @@ if __name__ == '__main__':
         for gold_item in gold_items:
             phrases_in_gold_docs.append(rag.get_phrases_in_doc_str(gold_item))
 
-        if args.dataset in ['hotpotqa', '2wikimultihopqa', 'hotpotqa_train']:
+        if ircot_config.corpus_name in ['hotpotqa', '2wikimultihopqa', 'hotpotqa_train']:
             sample['supporting_docs'] = [item for item in sample['supporting_facts']]
         else:
             sample['supporting_docs'] = [item for item in sample['paragraphs'] if item['is_supporting']]
@@ -296,10 +289,10 @@ if __name__ == '__main__':
         results.append(sample)
 
         if (sample_idx + 1) % 10 == 0:
-            with open(output_path, 'w') as f:
+            with open(ircot_config.output_path, 'w') as f:
                 json.dump(results, f)
 
     # save results
-    with open(output_path, 'w') as f:
+    with open(ircot_config.output_path, 'w') as f:
         json.dump(results, f)
-    print(f'Saved {len(results)} results to {output_path}')
+    print(f'Saved {len(results)} results to {ircot_config.output_path}')
